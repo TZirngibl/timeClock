bcf73628e044e0b4bc155fd4ba2ffe1faa789bf9
N03235893 <hoang_hoang5309@hotmail.com>
2018-03-30 14:33:55 -0400

    Full v1

---
+++ login-logout/logindata.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+    $db = mysqli_connect("localhost","root","","project1");
+    if(isset($_POST['login'])){
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+        $sql = "SELECT * FROM manager WHERE email='$email'";
+        $result = $db-> query($sql);
+        if($result-> num_rows > 0){
+                while($row = $result-> fetch_assoc()){
+                    if(password_verify($pass, $row['pass'])){
+                        if($row["level"] == 'm'){
+                            $_SESSION["nameofuser"] = $row["name"];
+                            $_SESSION['login'] = true;
+                            header("location:../home.php");
+                        }
+                        else
+                        {
+                            $_SESSION['errors'] = "You Are Not Admin";
+                            header("location:login.php");
+                        }
+                    }
+                    else
+                    {
+                        $_SESSION['errors'] = "Wrong Username or Password";
+                        header("location:login.php");
+                    }
+                }
+            }
+        else
+        {
+            $_SESSION['errors'] = "Wrong Username or Password";
+            header("location:login.php");
+        }
+    }
+?>
