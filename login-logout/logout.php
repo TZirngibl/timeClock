b70811a7f93d8e2887e0baccf0e3d3084935c9cb
Hoang Nguyen <hoang_hoang5309@hotmail.com>
2018-03-31 17:46:30 -0400

    delete

--- login-logout/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if(isset($_POST['login'])){
-        $_SESSION['login'] = false;
-        $_SESSION['nameofuser'] = "empty";
-        header("location:login.php");
-    }
-?>
