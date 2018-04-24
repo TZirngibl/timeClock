59a671200f40d5e04d3595dfea30c36f3a61aa08
Tim Zirngibl <Zirngibt1@hawkmail.newpaltz.edu>
2018-04-24 13:22:48 -0400

    fixed hamburger menus
    fixed hamburger menus

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         <div id="page-content-wrapper">
             
                 <div id="home-page-wrapper">
-                <!--NAVBAR-->
+                <!--EMPLOYEE NAVBAR-->
                     <nav class="navbar navbar-expand-lg navbar-dark" id="homenavbar">
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
@@ -86,11 +86,11 @@
                                 </div>
                                 <!-- MANAGER NAVBAR -->
                                 <nav class="navbar navbar-expand-lg navbar-dark" id="homenavbar">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <a class="navbar-brand" href="#">Managers</a>
-                            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                                 <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#addManager">Add Manager</a></li>
                                 </ul>
@@ -127,11 +127,11 @@
                         <div id="reportspage">
                             <!--REPORTS NAVBAR-->
                             <nav class="navbar navbar-expand-lg navbar-dark bg-success" id="reportnavbar">
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                                     <span class="navbar-toggler-icon"></span>
                                 </button>
                                 <a class="navbar-brand" href="#">Reports</a>
-                                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
                                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                                         <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#filterModal">Generate Report(s) <span class="sr-only">(current)</span></a></li>
                                         <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#addClock">Add Record(s)</a></li>
