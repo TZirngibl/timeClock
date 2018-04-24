1e858fe1b35a31a662f33efe7f5451e6268efc61
Tim Zirngibl <Zirngibt1@hawkmail.newpaltz.edu>
2018-04-24 11:26:01 -0400

    new login
    new login

--- PHP-folder/login_logout/login.php
+++ PHP-folder/login_logout/login.php
@@ -9,21 +9,81 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRST Clock Login</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-        <div class="form">
-            <img src="../../Art/welcome.gif" id = "welcome">
-            <div class="message">
-                <?php 
-                    echo '<font color="red">'. $_SESSION['errors'] .'</font>';
-                    $_SESSION['errors'] = "";
-                ?>
-            </div>
-            <form method="post" action="login_data.php" class="login-form">
-                <input type="text"      name="email"     placeholder="Your Email"/>
-                <input type="password"  name="pass"     placeholder="password"/>
-                <input id="login" type="submit" name="login" value="login"/> 
-            </form>
-        </div>
+        <div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100">
+				<form method="post" action="login_data.php" class="login100-form validate-form">
+					<span class="login100-form-logo">
+						<i src="" width="50px" height="75px"></i>
+					</span>
+
+					<span class="login100-form-title p-b-34 p-t-27">
+						CRST Timeclock
+                    </span>
+                    <div class="message">
+                       <?php 
+                         echo '<font color="red">'. $_SESSION['errors'] .'</font>';
+                         $_SESSION['errors'] = "";
+                        ?>
+                     </div>
+					<div class="wrap-input100 validate-input" data-validate = "Enter E-mail Address">
+						<input class="input100" type="text" name="email" placeholder="Enter E-mail Address">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+                    </div>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" type="submit" name="login">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div id="dropDownSelect1"></div>
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
 </body>
 </html>
